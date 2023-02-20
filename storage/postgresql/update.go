49990ad8209298d09c944cc24c8e42126fd326c1
weibogao <45678072+cryptowizard0@users.noreply.github.com>
2023-02-20 21:27:08 +0800

    update itemid after saved event to ar

---
+++ storage/postgresql/update.go
@@ -0,0 +1,43 @@
+package postgresql
+
+import (
+	"encoding/json"
+
+	"github.com/nbd-wtf/go-nostr"
+	"github.com/permadao/ArNostr-relayer/storage"
+)
+
+func (b *PostgresBackend) UpdateItemId(evt *nostr.Event, itemid string) error {
+	// react to different kinds of events
+	if evt.Kind == nostr.KindSetMetadata || evt.Kind == nostr.KindContactList || (10000 <= evt.Kind && evt.Kind < 20000) {
+		// delete past events from this user
+		b.DB.Exec(`DELETE FROM event WHERE pubkey = $1 AND kind = $2`, evt.PubKey, evt.Kind)
+	} else if evt.Kind == nostr.KindRecommendServer {
+		// delete past recommend_server events equal to this one
+		b.DB.Exec(`DELETE FROM event WHERE pubkey = $1 AND kind = $2 AND content = $3`,
+			evt.PubKey, evt.Kind, evt.Content)
+	}
+
+	// insert or update itemid
+	tagsj, _ := json.Marshal(evt.Tags)
+	res, err := b.DB.Exec(`
+        INSERT INTO event (id, pubkey, created_at, kind, tags, content, sig, itemid)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+		ON CONFLICT (id) DO UPDATE 
+		SET itemid = $8
+    `, evt.ID, evt.PubKey, evt.CreatedAt.Unix(), evt.Kind, tagsj, evt.Content, evt.Sig, itemid)
+	if err != nil {
+		return err
+	}
+
+	nr, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	if nr == 0 {
+		return storage.ErrDupEvent
+	}
+
+	return nil
+}
